6675f3e3ca7390d2ec7e4d6c3a330312a19e2310
xSkyWard <SkyWards1@outlook.com>
2015-12-14 01:30:23 +0300

    Добавил пакет PlayFail

--- Auth/ServerPackets/PlayFail.cs
+++ Auth/ServerPackets/PlayFail.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Auth.ServerPackets
+﻿namespace Auth.ServerPackets
 {
-    class PlayFail
+    public class PlayFail : ServerPacket
     {
+        // format c
+        //
+        // 07 
+        // 34 0b 00 00 
+        // 14 16 0d 00
+        // align+chksum  
+        // 00 00 00 00 
+        // 00 00 67 07 
+        // 25 1d 6a 6b 66 6b 64 
+
+        public static int REASON_TOO_MANY_PLAYERS = 0x0f;
+        public static int REASON1 = 0x01; // account in use
+        public static int REASON2 = 0x02;
+        public static int REASON3 = 0x03;
+        public static int REASON4 = 0x04;
+
+        public PlayFail(int Reason)
+        {
+            WriteC(0x06);
+            WriteC(Reason); // too many players on server
+        }
+
+        public override byte[] GetContent()
+        {
+            return GetBytes();
+        }
     }
 }
