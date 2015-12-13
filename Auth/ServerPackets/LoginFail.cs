b1eb7b7fa25d0f2c9e742f3105bea4a4189719b8
xSkyWard <SkyWards1@outlook.com>
2015-12-14 01:34:56 +0300

    Добавил пакет LoginFail

--- Auth/ServerPackets/LoginFail.cs
+++ Auth/ServerPackets/LoginFail.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Auth.ServerPackets
+﻿namespace Auth.ServerPackets
 {
-    class LoginFail
+    public class LoginFail : ServerPacket
     {
+        public static int REASON_ACCOUNT_BANNED = 0x09;
+        public static int REASON_ACCOUNT_IN_USE = 0x07;
+        public static int REASON_ACCESS_FAILED = 0x04;
+        public static int REASON_USER_OR_PASS_WRONG = 0x03;
+        public static int REASON_PASS_WRONG = 0x02;
+        public static int REASON_SYSTEM_ERROR = 0x01;
+
+        public LoginFail(int Reason)
+        {
+            WriteC(0x01);
+            WriteD(Reason);
+        }
+
+        public override byte[] GetContent()
+        {
+            return GetBytes();
+        }
     }
 }
