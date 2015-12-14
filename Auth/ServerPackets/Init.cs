27bcadafa129572d0489acedb58068fbd94b660c
xSkyWard <SkyWards1@outlook.com>
2015-12-15 01:25:06 +0300

    Добавил пакет Init

--- Auth/ServerPackets/Init.cs
+++ Auth/ServerPackets/Init.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Auth.ServerPackets
 {
-    class Init
+    public class Init
     {
+        private static byte[] Content =
+        {
+            0x00,
+            0x9c,
+            0x77,
+            0xed,
+            0x03,
+            0x5a,
+            0x78,
+            0x00,
+            0x00 };
+
+        public virtual byte[] GetContent()
+        {
+            return Content;
+        }
+        public virtual int GetLength()
+        {
+            return Content.Length + 2;
+        }
     }
 }
